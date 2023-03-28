b5573ecec2724790adba8086b6f6b1ca81dd1ed0
Jopa1474 <Jopa1474@gmail.com>
2023-03-27 18:12:22 -0600

    primer commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module juego.buscaminas {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens juego.buscaminas to javafx.fxml;
     exports juego.buscaminas;
 }
