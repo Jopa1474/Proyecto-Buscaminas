9b00036d3ec3f654d0b37e6eef060d9c2cac4adc
Jopa1474 <Jopa1474@gmail.com>
2023-04-11 00:18:38 -0600

    Sexto Commit 1.1 (ControlArduino, Observer, MoverCasilla)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module juego.buscaminas {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.fazecast.jSerialComm;
+    requires java.desktop;
 
 
     opens juego.buscaminas to javafx.fxml;
